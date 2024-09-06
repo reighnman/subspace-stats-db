80a4182047926a97008714ba3c4b2a5586c6f41e
gigamon <gigamon@hotmail.com>
2024-09-06 18:34:00 -0400

    Fix an issue in the get_or_insert_stat_periods function for when a stat_tracking record is not set to auto generate. Also, handle the case if a stat_period_type_id is added without adding logic to generate it in the function.

--- Schemas/ss/Functions/get_or_insert_stat_periods.sql
+++ Schemas/ss/Functions/get_or_insert_stat_periods.sql
@@ -11,6 +11,13 @@ as
 $$
 
 /*
+Gets the stat periods for a given game type and timestamp.
+Stat periods that do not exist yet are inserted if it is configured in the stat_tracking table to auto generate.
+
+Parameters:
+p_game_type_id - The game type to get stat periods for.
+p_as_of - The timestamp to get stat periods for.
+
 Usage:
 select * from get_or_insert_stat_periods(4, current_timestamp);
 
@@ -27,7 +34,9 @@ with cte_all_periods as(
 		on st.stat_tracking_id = sp.stat_tracking_id
 			and sp.period_range @> p_as_of
 	where st.game_type_id = p_game_type_id
-		and (st.is_auto_generate_period = true or sp.stat_period_id is not null)
+		and (sp.stat_period_id is not null
+			or (sp.stat_period_id is null and st.is_auto_generate_period = true)
+		)
 )
 ,cte_insert_stat_period as(
 	insert into stat_period(
@@ -35,20 +44,26 @@ with cte_all_periods as(
 		,period_range
 	)
 	select
-		 cap.stat_tracking_id
-		,case cap.stat_period_type_id
-			when 0 then( -- Forever
-				select tstzrange(null, null)
-			)
-			when 1 then( -- Monthly
-				select tstzrange(dt.start, dt.start + '1 month'::interval, '[)')
-				from(
-					select date_trunc('month', p_as_of) as start
-				) as dt
-			)
-		 end as period_range
-	from cte_all_periods as cap
-	where cap.stat_period_id is null -- any that are null must have is_auto_generate_period = true
+		 dt2.stat_tracking_id
+		,dt2.period_range
+	from(
+		select
+			 cap.stat_tracking_id
+			,case cap.stat_period_type_id
+				when 0 then( -- Forever
+					select tstzrange(null, null)
+				)
+				when 1 then( -- Monthly
+					select tstzrange(dt.start, dt.start + '1 month'::interval, '[)')
+					from(
+						select date_trunc('month', p_as_of) as start
+					) as dt
+				)
+			 end as period_range
+		from cte_all_periods as cap
+		where cap.stat_period_id is null -- any that are null must have is_auto_generate_period = true
+	) as dt2
+	where dt2.period_range is not null -- only if we know how to generate a new range for the stat_period_type_id
 	returning
 		 stat_period_id
 		,stat_tracking_id
