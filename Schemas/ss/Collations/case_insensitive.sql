4e66af60fdff27276de1201501175fd884e09af3
gigamon <gigamon@hotmail.com>
2025-03-24 16:17:21 -0400

    Script to include schema name and owner.

--- Schemas/ss/Collations/case_insensitive.sql
+++ Schemas/ss/Collations/case_insensitive.sql
@@ -2,4 +2,7 @@
 
 -- DROP COLLATION IF EXISTS ss.case_insensitive;
 
-CREATE COLLATION IF NOT EXISTS case_insensitive (provider = icu, locale = 'und-u-ks-level2', deterministic = false);
+CREATE COLLATION IF NOT EXISTS ss.case_insensitive (provider = icu, locale = 'und-u-ks-level2', deterministic = false);
+
+ALTER COLLATION ss.case_insensitive
+    OWNER TO ss_developer;
