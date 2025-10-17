5a7cae836fbd86201f57660e00c34812a675a514
gigamon <gigamon@hotmail.com>
2025-10-17 17:02:48 -0400

    game_type.game_mode_id to not be nullable

--- Schemas/ss/Tables/game_type.sql
+++ Schemas/ss/Tables/game_type.sql
@@ -6,7 +6,7 @@ CREATE TABLE IF NOT EXISTS ss.game_type
 (
     game_type_id bigint NOT NULL GENERATED ALWAYS AS IDENTITY ( INCREMENT 1 START 1 MINVALUE 1 MAXVALUE 9223372036854775807 CACHE 1 ),
     game_type_name character varying(128) COLLATE pg_catalog."default" NOT NULL,
-    game_mode_id bigint,
+    game_mode_id bigint NOT NULL,
     CONSTRAINT game_type_pkey PRIMARY KEY (game_type_id),
     CONSTRAINT game_type_game_type_name_game_type_id_game_mode_id_key UNIQUE (game_type_name)
         INCLUDE(game_type_id, game_mode_id),
